9328c255c7d86be7f790606d02663ef6b91506cf
Sudarenko Sergey <Vipgaz2@gmail.com>
2024-05-15 19:24:02 +0200

    up

--- HW_3_2.py
+++
@@ -1,9 +0,0 @@
-GET /other-19 HTTP/1.1
-Host: www.example.com
-User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.0; ru; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-Accept-Language: ru,en-us;q=0.7,en;q=0.3
-Accept-Encoding: gzip,deflate
-Accept-Charset: windows-1251,utf-8;q=0.7,*;q=0.7
-Keep-Alive: 300
-Connection: keep-alive
