a78014fe00ea740ff32250cec9b4b49ae1d33d7b
Sudarenko Sergey <Vipgaz2@gmail.com>
2024-05-16 13:14:11 +0200

    New_clear

--- HW_4_1.py/main.py
+++ HW_4_1.py/main.py
@@ -1,11 +1,15 @@
 import os
 import json
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_socketio import SocketIO, emit
 import threading
 
-app = Flask(__name__)
+# Вказуємо правильні шляхи до папок зі статичними файлами та шаблонами
+static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+app = Flask(__name__, static_folder=static_path, template_folder=template_path)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
@@ -15,13 +19,11 @@ if not os.path.exists('storage/data.json'):
     with open('storage/data.json', 'w') as f:
         json.dump({}, f)
 
-templates_path = os.path.join(os.path.dirname(__file__), 'templates')
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/message')
+@app.route('/message.html')
 def message():
     return render_template('message.html')
 
@@ -49,4 +51,11 @@ if __name__ == '__main__':
     flask_thread.start()
     socket_thread = threading.Thread(target=run_socket_server)
     socket_thread.start()
+
+
+
+
+
+
+
 
